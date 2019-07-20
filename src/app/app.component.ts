7920fbc33baebbb50f6617dc3b793a0c2801d1a0
Bruno da Silva Joao <brunodasj@gmail.com>
2019-07-20 08:30:04 -0300

    feat(AppComponntOnPush): add onPush strategy to appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { environment } from '@env/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
   title = environment.appName;
